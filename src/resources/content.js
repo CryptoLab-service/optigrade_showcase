8173324cfcf821e27d133de0f6cfd5981662adc5
TOLUWALOPE JOHN OLUWALOWO <58827223+CryptoLab-service@users.noreply.github.com>
2025-07-01 13:47:14 +0000

    Reduce subline font size on landing page

--- src/resources/content.js
+++ src/resources/content.js
@@ -63,7 +63,7 @@ const home = {
   },
   subline: (
     <>
-      I'm Toluwalope John O., a designer and founder of <Logo icon="/trademarks/wordmark-dark.svg" style={{ display: "inline-flex", top: "0.25em", marginLeft: "-0.25em" }}/>, where I craft intuitive and purposeful
+      I'm Toluwalope John, a designer and founder of <Logo icon="/trademarks/wordmark-dark.svg" style={{ display: "inline-flex", top: "0.25em", marginLeft: "-0.25em" }}/>, where I craft intuitive and purposeful
       <br /> user interfaces and experiences. Through ongoing exploration, I prototype new ideas and build my own projects and tools—like Optigrade, a tool built to simplify and streamline educational workflows for smarter academic  performance.
     </>
   ),
