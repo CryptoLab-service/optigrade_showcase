33397850841d05633fdad7d1b957cf87a7eff8f4
TOLUWALOPE JOHN OLUWALOWO <58827223+CryptoLab-service@users.noreply.github.com>
2025-07-03 16:16:46 +0100

    Update content.js

--- src/resources/content.js
+++ src/resources/content.js
@@ -59,7 +59,7 @@ const home = {
   featured: {
     display: true,
     title: <>Recent project: <strong className="ml-4">OptiGrade Mobile App</strong></>,
-    href: "/work/building-once-ui-a-customizable-design-system",
+    href: "/work/optigrade-academic-intelligence",
   },
   subline: (
   <>
