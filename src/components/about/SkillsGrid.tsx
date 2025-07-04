56434763c728367658d7714657b3dfcef2cfe5ef
TOLUWALOPE JOHN OLUWALOWO <58827223+CryptoLab-service@users.noreply.github.com>
2025-07-04 18:00:28 +0100

    Update SkillsGrid.tsx

--- src/components/about/SkillsGrid.tsx
+++ src/components/about/SkillsGrid.tsx
@@ -2,11 +2,11 @@ import { Flex } from "@once-ui-system/core";
 import Image from "next/image";
 export const SkillsGrid = () => (
   <Flex wrap horizontal="center" gap="24" paddingY="32">
-    <img src="/logos/figma.png" alt="Figma" width="40" />
-    <img src="/logos/notion.png" alt="Notion" width="40" />
-    <img src="/logos/photoshop.png" alt="Photoshop" width="40" />
-    <img src="/logos/xd.png" alt="Adobe XD" width="40" />
-    <img src="/logos/jupyter.png" alt="Jupyter" width="40" />
-    <img src="/logos/looker.png" alt="Looker Studio" width="40" />
+    <image src="/logos/figma.png" alt="Figma" width="40" />
+    <image src="/logos/notion.png" alt="Notion" width="40" />
+    <image src="/logos/photoshop.png" alt="Photoshop" width="40" />
+    <image src="/logos/xd.png" alt="Adobe XD" width="40" />
+    <image src="/logos/jupyter.png" alt="Jupyter" width="40" />
+    <image src="/logos/looker.png" alt="Looker Studio" width="40" />
   </Flex>
 );
