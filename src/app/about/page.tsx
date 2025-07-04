54be02ca3d62e34ff200f5ad146fc7c3d0f315d7
TOLUWALOPE JOHN OLUWALOWO <58827223+CryptoLab-service@users.noreply.github.com>
2025-07-04 18:56:56 +0100

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -52,7 +52,7 @@ export default function About() {
     {
       title: about.certification.title,
       display: about.certification.display,
-      items: about.certification.skills.map((award) => certification.title),
+      items: about.certification.institutions.map((award) => award.name)
     },
   ];
   return (
