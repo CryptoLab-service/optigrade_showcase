bdc039f0a41604c236e7de1484e720ab349816c3
TOLUWALOPE JOHN OLUWALOWO <58827223+CryptoLab-service@users.noreply.github.com>
2025-07-04 22:41:54 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -85,18 +85,6 @@ export default function Home() {
         </Column>
       </Column>
 
-      {/* 🧰 Skills & Tools */}
-      <RevealFx translateY="16" delay={0.5}>
-        <Flex horizontal="center" wrap gap="24" paddingY="32">
-          <Image src="/logos/figma.png" alt="Figma" width={40} height={40} style={{ objectFit: "contain" }} />
-          <Image src="/logos/notion.png" alt="Notion" width={40} height={40} style={{ objectFit: "contain" }} />
-          <Image src="/logos/photoshop.png" alt="Photoshop" width={40} height={40} style={{ objectFit: "contain" }} />
-          <Image src="/logos/xd.png" alt="Adobe XD" width={40} height={40} style={{ objectFit: "contain" }} />
-          <Image src="/logos/microsoft.png" alt="Microsoft" width={40} height={40} style={{ objectFit: "contain" }} />
-          <Image src="/logos/cisco.png" alt="Cisco" width={40} height={40} style={{ objectFit: "contain" }} />
-        </Flex>
-      </RevealFx>
-
       {/* 🖼️ Featured Projects */}
       <RevealFx translateY="16" delay={0.6}>
         <Projects range={[1, 1]} />
