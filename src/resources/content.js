d08417926c9ac114a4443c3caf74184a339550f4
TOLUWALOPE JOHN OLUWALOWO <58827223+CryptoLab-service@users.noreply.github.com>
2025-07-04 22:53:01 +0100

    Update content.js

--- src/resources/content.js
+++ src/resources/content.js
@@ -258,6 +258,30 @@ const about = {
         </Flex>
       </RevealFx>
 
+      {/* 📄 Embedded Google CV */}
+      <RevealFx translateY="24" delay={1}>
+        <Heading variant="display-strong-xs" paddingBottom="16">
+          View My CV
+        </Heading>
+        <iframe
+          src="https://drive.google.com/file/d/15hR0lR5yNWEsGFyNOXnYyXdHXkPqohND/view?usp=sharing"
+          width="100%"
+          height="640"
+          style={{
+            backdropFilter: "blur(16px)",
+            width: "auto",
+            maxWidth: "100%",
+            minWidth: "fit-content",
+            display: "flex",
+            flexWrap: "wrap",
+            alignItems: "center",
+            justifyContent: "center",
+            gap: "12px",
+          }}
+          allow="autoplay"
+        ></iframe>
+      </RevealFx>
+
 const blog = {
   path: "/blog",
   label: "Blog",
