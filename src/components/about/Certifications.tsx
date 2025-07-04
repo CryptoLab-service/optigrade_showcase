797ab221a7d2f33960651e1401a791aa9f8ab8ba
TOLUWALOPE JOHN OLUWALOWO <58827223+CryptoLab-service@users.noreply.github.com>
2025-07-04 19:15:28 +0100

    Update Certifications.tsx

--- src/components/about/Certifications.tsx
+++ src/components/about/Certifications.tsx
@@ -1,12 +1,13 @@
 import { Heading, Flex } from "@once-ui-system/core";
+import Image from "next/image";
 
 export const Certifications = () => (
   <>
     <Heading variant="display-strong-xs" paddingBottom="8">
       Certified By
     </Heading>
-    <Flex wrap gap="28" horizontal="center" paddingBottom="32">
-      <image src="/logos/alx.png" width="80" alt="ALX" />
+    <Flex wrap gap="xl" horizontal="center" paddingBottom="32">
+      <Image src="/logos/alx.png" alt="ALX" width={80} height={80} />
       <image src="/logos/3mtt.png" width="80" alt="3MTT Nigeria" />
       <image src="/logos/accessbank.png" width="80" alt="Access Bank" />
       <image src="/logos/microsoft.png" width="80" alt="Microsoft Learn" />
