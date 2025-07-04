317ecc45f879890c45bb9623cce5daa4f83a4740
TOLUWALOPE JOHN OLUWALOWO <58827223+CryptoLab-service@users.noreply.github.com>
2025-07-04 22:17:22 +0100

    Update SkillsGrid.tsx

--- src/components/about/SkillsGrid.tsx
+++ src/components/about/SkillsGrid.tsx
@@ -1,12 +1,13 @@
 import { Flex } from "@once-ui-system/core";
 import Image from "next/image";
+
 export const SkillsGrid = () => (
   <Flex wrap horizontal="center" gap="24" paddingY="32">
-    <image src="/logos/figma.png" alt="Figma" width="40" />
-    <image src="/logos/notion.png" alt="Notion" width="40" />
-    <image src="/logos/photoshop.png" alt="Photoshop" width="40" />
-    <image src="/logos/xd.png" alt="Adobe XD" width="40" />
-    <image src="/logos/jupyter.png" alt="Jupyter" width="40" />
-    <image src="/logos/looker.png" alt="Looker Studio" width="40" />
+    <Image src="/logos/figma.png" alt="Figma" width={40} height={40} style={{ objectFit: "contain" }} />
+    <Image src="/logos/notion.png" alt="Notion" width={40} height={40} style={{ objectFit: "contain" }} />
+    <Image src="/logos/photoshop.png" alt="Photoshop" width={40} height={40} style={{ objectFit: "contain" }} />
+    <Image src="/logos/xd.png" alt="Adobe XD" width={40} height={40} style={{ objectFit: "contain" }} />
+    <Image src="/logos/jupyter.png" alt="Jupyter" width={40} height={40} style={{ objectFit: "contain" }} />
+    <Image src="/logos/looker.png" alt="Looker Studio" width={40} height={40} style={{ objectFit: "contain" }} />
   </Flex>
 );
