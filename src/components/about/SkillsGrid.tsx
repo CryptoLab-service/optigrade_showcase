681bc34f2d34d9669328ecdd4097a8f4d91862ee
TOLUWALOPE JOHN OLUWALOWO <58827223+CryptoLab-service@users.noreply.github.com>
2025-07-04 23:58:42 +0100

    Update SkillsGrid.tsx

--- src/components/about/SkillsGrid.tsx
+++ src/components/about/SkillsGrid.tsx
@@ -1,13 +1,15 @@
-import { Flex } from "@once-ui-system/core";
-import Image from "next/image";
+import { Flex } from '@once-ui-system/core';
+import Image from 'next/image';
 
 export const SkillsGrid = () => (
   <Flex wrap horizontal="center" gap="24" paddingY="32">
-    <Image src="/logos/figma.png" alt="Figma" width={40} height={40} style={{ objectFit: "contain" }} />
-    <Image src="/logos/notion.png" alt="Notion" width={40} height={40} style={{ objectFit: "contain" }} />
-    <Image src="/logos/photoshop.png" alt="Photoshop" width={40} height={40} style={{ objectFit: "contain" }} />
-    <Image src="/logos/xd.png" alt="Adobe XD" width={40} height={40} style={{ objectFit: "contain" }} />
-    <Image src="/logos/jupyter.png" alt="Jupyter" width={40} height={40} style={{ objectFit: "contain" }} />
-    <Image src="/logos/looker.png" alt="Looker Studio" width={40} height={40} style={{ objectFit: "contain" }} />
+    <Image src="/logos/figma.png" alt="Figma" width={40} height={40} style={{ objectFit: 'contain' }} />
+    <Image src="/logos/notion.png" alt="Notion" width={40} height={40} style={{ objectFit: 'contain' }} />
+    <Image src="/logos/photoshop.png" alt="Photoshop" width={40} height={40} style={{ objectFit: 'contain' }} />
+    <Image src="/logos/xd.png" alt="Adobe XD" width={40} height={40} style={{ objectFit: 'contain' }} />
+    <Image src="/logos/jupyter.png" alt="Jupyter" width={40} height={40} style={{ objectFit: 'contain' }} />
+    <Image src="/logos/looker.png" alt="Looker Studio" width={40} height={40} style={{ objectFit: 'contain' }} />
+    <Image src="/logos/microsoft.png" alt="Microsoft" width={40} height={40} style={{ objectFit: 'contain' }} />
+    <Image src="/logos/cisco.png" alt="Cisco" width={40} height={40} style={{ objectFit: 'contain' }} />
   </Flex>
 );
