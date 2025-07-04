a16a429d98841d9f940f2934b6645cd895f6e0ed
TOLUWALOPE JOHN OLUWALOWO <58827223+CryptoLab-service@users.noreply.github.com>
2025-07-04 22:47:46 +0100

    Update content.js

--- src/resources/content.js
+++ src/resources/content.js
@@ -246,6 +246,18 @@ const about = {
   },
 };
 
+      {/* 🧰 Skills & Tools */}
+      <RevealFx translateY="16" delay={0.5}>
+        <Flex horizontal="center" wrap gap="24" paddingY="32">
+          <Image src="/logos/figma.png" alt="Figma" width={40} height={40} style={{ objectFit: "contain" }} />
+          <Image src="/logos/notion.png" alt="Notion" width={40} height={40} style={{ objectFit: "contain" }} />
+          <Image src="/logos/photoshop.png" alt="Photoshop" width={40} height={40} style={{ objectFit: "contain" }} />
+          <Image src="/logos/xd.png" alt="Adobe XD" width={40} height={40} style={{ objectFit: "contain" }} />
+          <Image src="/logos/microsoft.png" alt="Microsoft" width={40} height={40} style={{ objectFit: "contain" }} />
+          <Image src="/logos/cisco.png" alt="Cisco" width={40} height={40} style={{ objectFit: "contain" }} />
+        </Flex>
+      </RevealFx>
+
 const blog = {
   path: "/blog",
   label: "Blog",
