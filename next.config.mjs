de97db80711b7833ca2f976add573d3ba0c40cdd
TOLUWALOPE JOHN OLUWALOWO <58827223+CryptoLab-service@users.noreply.github.com>
2025-07-05 19:19:56 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,19 @@ import mdx from "@next/mdx";
 
 const withMDX = mdx({
   extension: /\.mdx?$/,
-  options: {},
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ["ts", "tsx", "md", "mdx"],
+  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
   transpilePackages: ["next-mdx-remote"],
+  experimental: {
+    mdxRs: true, // Enable the new Rust-based MDX compiler
+  },
   sassOptions: {
     compiler: "modern",
     silenceDeprecations: ["legacy-js-api"],
