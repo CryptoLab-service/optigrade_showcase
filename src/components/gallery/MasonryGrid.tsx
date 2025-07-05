467ba677a240ac7975e6dc6ceb3f3240ed672240
TOLUWALOPE JOHN OLUWALOWO <58827223+CryptoLab-service@users.noreply.github.com>
2025-07-05 21:58:10 +0100

    Update MasonryGrid.tsx

--- src/components/gallery/MasonryGrid.tsx
+++ src/components/gallery/MasonryGrid.tsx
@@ -1,34 +1,28 @@
 "use client";
 
-import Masonry from "react-masonry-css";
 import { Media } from "@once-ui-system/core";
 import styles from "./Gallery.module.scss";
 import { gallery } from "@/resources";
 
 export default function MasonryGrid() {
-  const breakpointColumnsObj = {
-    default: 2,
-    720: 1,
-  };
-
   return (
-    <Masonry
-      breakpointCols={breakpointColumnsObj}
-      className={styles.masonryGrid}
-      columnClassName={styles.masonryGridColumn}
-    >
+    <div className={styles.cssGrid}>
       {gallery.images.map((image, index) => (
-        <Media
-          priority={index < 10}
-          sizes="(max-width: 560px) 100vw, 50vw"
-          key={index}
-          radius="m"
-          aspectRatio={image.orientation === "horizontal" ? "16 / 9" : "3 / 4"}
-          src={image.src}
-          alt={image.alt}
+        <div 
+          key={index} 
           className={styles.gridItem}
-        />
+          data-orientation={image.orientation}
+        >
+          <Media
+            priority={index < 10}
+            sizes="(max-width: 560px) 100vw, 50vw"
+            radius="m"
+            aspectRatio={image.orientation === "horizontal" ? "16 / 9" : "3 / 4"}
+            src={image.src}
+            alt={image.alt}
+          />
+        </div>
       ))}
-    </Masonry>
+    </div>
   );
 }
