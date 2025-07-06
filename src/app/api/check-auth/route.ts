a42c9bd7d306781046e42a9bb9766e6f30607a49
TOLUWALOPE JOHN OLUWALOWO <58827223+CryptoLab-service@users.noreply.github.com>
2025-07-06 16:37:14 +0100

    Update route.ts

--- src/app/api/check-auth/route.ts
+++ src/app/api/check-auth/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import * as cookie from "cookie";
+import { serialize } from "cookie"; // Import the specific function you need
 
 export async function GET(request: NextRequest) {
   const cookieHeader = request.headers.get("cookie") || "";
