6d9ca2b61249c0bb33c7e8458baa9a155e3f6e7b
TOLUWALOPE JOHN OLUWALOWO <58827223+CryptoLab-service@users.noreply.github.com>
2025-07-07 01:40:55 +0100

    Update next.d.ts

--- src/types/next.d.ts
+++ src/types/next.d.ts
@@ -2,7 +2,7 @@ import { Metadata } from 'next';
 
 declare module 'next' {
   export interface PageProps {
-    params: { [key: string]: string | string[] };
+    params: { [key: string]: string };
     searchParams?: { [key: string]: string | string[] | undefined };
   }
   
