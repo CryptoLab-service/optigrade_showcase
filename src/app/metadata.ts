80c3de4e2df2d3ffe881eee85c5458e30179d78b
TOLUWALOPE JOHN OLUWALOWO <58827223+CryptoLab-service@users.noreply.github.com>
2025-07-07 20:53:26 +0100

    Create metadata.ts

---
+++ src/app/metadata.ts
@@ -0,0 +1,12 @@
+import { Meta } from '@once-ui-system/core';
+import { home, baseURL } from '@/resources';
+
+export async function generateMetadata() {
+  return Meta.generate({
+    title: home.title,
+    description: home.description,
+    baseURL: baseURL,
+    path: home.path,
+    image: home.image,
+  });
+}
