c3658ff0fb056ab3ebf55fcea3f508c4fe686fbf
TOLUWALOPE JOHN OLUWALOWO <58827223+CryptoLab-service@users.noreply.github.com>
2025-07-07 17:54:04 +0100

    Create global-error.jsx

---
+++ src/app/global-error.jsx
@@ -0,0 +1,13 @@
+// src/app/global-error.jsx
+'use client';
+
+export default function GlobalError({ error, reset }) {
+  return (
+    <html>
+      <body>
+        <h2>Something went wrong globally!</h2>
+        <button onClick={() => reset()}>Try again</button>
+      </body>
+    </html>
+  );
+}
