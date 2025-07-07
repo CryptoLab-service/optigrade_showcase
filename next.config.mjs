d7ccc1d0f0e7340c393be61e9e31f77593413d76
TOLUWALOPE JOHN OLUWALOWO <58827223+CryptoLab-service@users.noreply.github.com>
2025-07-07 17:30:49 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -20,7 +20,7 @@ const nextConfig = {
     silenceDeprecations: ["legacy-js-api"],
   },
   typescript: {
-    ignoreBuildErrors: true, // This is the key fix
+    ignoreBuildErrors: true, // Add this line
   },
 };
 
