3ed88d8f770d09a51705158ef95c2240c70bd1d6
TOLUWALOPE JOHN OLUWALOWO <58827223+CryptoLab-service@users.noreply.github.com>
2025-07-08 00:48:03 +0100

    Create ThemeProvider.tsx

---
+++ src/app/ThemeProvider.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { createContext, useContext, useEffect, useState } from 'react';
+
+const ThemeContext = createContext({
+  theme: 'system',
+  setTheme: (theme: string) => {}
+});
+
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+  const [theme, setTheme] = useState('system');
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem('data-theme') || 'system';
+    setTheme(storedTheme);
+  }, []);
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export const useTheme = () => useContext(ThemeContext);
