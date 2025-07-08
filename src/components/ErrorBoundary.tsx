664e2df4ffe3a440856fb9694bb430c9c21d06f3
TOLUWALOPE JOHN OLUWALOWO <58827223+CryptoLab-service@users.noreply.github.com>
2025-07-08 08:37:42 +0100

    Create ErrorBoundary.tsx

---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,36 @@
+// src/components/ErrorBoundary.tsx
+'use client';
+
+import { Component, ErrorInfo, ReactNode } from 'react';
+import { NotFound } from '@/components/NotFound';
+
+interface ErrorBoundaryProps {
+  children: ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+export class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(_: Error): ErrorBoundaryState {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error("Uncaught error:", error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <NotFound />;
+    }
+
+    return this.props.children;
+  }
+}
